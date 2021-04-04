62c2a97d155cd87c1c4f683a784ed8ffa5ef3179
mehditounsi <mehditounsi1997@gmail.com>
2021-04-04 20:43:06 +0100

    jwt auth 30% complete

--- redfield/src/app/app.module.ts
+++ redfield/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginpageComponent
+    AuthenticationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
